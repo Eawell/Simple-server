c2ee1b22eedb2764d768bb2b2d738d388016ae98
Eawell <eliot.southwell@challoners.org>
2020-10-29 13:50:40 +0000

    Skins

--- Menu.gd
+++ Menu.gd
@@ -1,7 +1,5 @@
 extends Control
 
-var x = 0
-var y = 0
 
 
 func _on_LineEdit_text_changed(new_text):
@@ -12,16 +10,8 @@ func _on_LineEdit_text_changed(new_text):
 func _on_LineEdit2_text_changed(new_text):
 	if new_text == "":
 		return
-	x = new_text
-	Network.data["pos"] = Vector2(x,y)
-
-func _on_LineEdit3_text_changed(new_text):
-	if new_text == "":
-		return
-	y = new_text
-	Network.data["pos"] = Vector2(x,y)
-
+	Network._ip = new_text
 
 func _on_Join_pressed():
 	Network.connect_to_server()
-	get_tree().change_scene("res://Game.tscn")
+

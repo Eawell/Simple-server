fd54dc3535108244dc32ec73070f5d780a9b6473
Eawell <eliot.southwell@challoners.org>
2020-10-29 13:56:55 +0000

    reset

--- Network.gd
+++ Network.gd
@@ -1,52 +1,67 @@
 extends Node
 
 const PORT = 4031
-const _IP = "127.0.0.1"
+var _ip = "127.0.0.1"
 
 var players = {}
-var data = {"nickname" : "", "pos" : Vector2()}
+var data = {"nickname" : "", "skin" : "white"}
 var taken = []
 
 func _ready():
 	get_tree().connect('network_peer_disconnected', self, '_on_player_disconnected')
 	get_tree().connect('network_peer_connected', self, '_on_player_connected')
 
+
 #i'm joining
 func connect_to_server():
+	print("connect_to_server")
 	get_tree().connect('connected_to_server', self, '_connected_to_server')
 	var peer = NetworkedMultiplayerENet.new()
-	peer.create_client(_IP, PORT)
+	peer.create_client(_ip, PORT)
 	get_tree().set_network_peer(peer)
 
 #i have joined
 func _connected_to_server():
+	print("_connected_to_server")
 	var local_player_id = get_tree().get_network_unique_id()
 	players[local_player_id] = data
+	get_tree().change_scene("res://Lobby.tscn")
 	rpc('_send_player_info', local_player_id, data)
 
 #everyone individually updates the players' info
 remote func _send_player_info(id, info):
+	print("_send_player_info")
 	players[id] = info
 	var new_player = load('res://Player.tscn').instance()
 	new_player.name = str(id)
 	new_player.set_network_master(id)
-	$'/root/Game/'.add_child(new_player)
-	new_player.init(info.nickname, info.pos, true)
-	
+	$'/root/Lobby/'.add_child(new_player)
+	new_player.init(info.nickname, info.skin, true)
+
 #if another player joined the game that i'm in, i request info
 func _on_player_connected(connected_player_id):
 	if (connected_player_id == 1):
 		return
-	if not(get_tree().is_network_server()):
-		var local_player_id = get_tree().get_network_unique_id()
-		rpc_id(1, '_request_player_info', local_player_id, connected_player_id)
+	var local_player_id = get_tree().get_network_unique_id()
+	rpc_id(1, '_request_player_info', local_player_id, connected_player_id)
 
-#if someone leaves, get rid of their data
 func _on_player_disconnected(id):
+	if taken.has(players[id]["skin"]):
+		taken.erase(players[id]["skin"])
 	players.erase(id)
 
-#func update_position(id, position):
-#	players[id].position = position
+func change_skin(id, old_skin, new_skin):
+	if !taken.has(new_skin):
+		rpc('changed_skin', id, old_skin, new_skin)
+	
+sync func changed_skin(id, old_skin, new_skin):
+	if old_skin != "white":
+		taken.erase(old_skin)
+	taken.append(new_skin)
+	players[id]["skin"] = new_skin
+
+remote func check_taken(new_taken):
+	taken = new_taken
 
 #ask for info of other player
 #remote func _request_player_info(request_from_id, player_id):
--- Player.gd
+++ Player.gd
@@ -1,38 +1,30 @@
 extends KinematicBody2D
 
-var SPEED = 10.0
-var skins = ["blue", "green", "pink", "red", "yellow"]
+var SPEED = 500.0
+var skins = ["blue", "green", "pink", "red", "yellow", "dew"]
 
-enum Direction { UP, DOWN, LEFT, RIGHT, NONE }
+enum Direction { UPRIGHT, UPLEFT, DOWNRIGHT, DOWNLEFT, UP, DOWN, LEFT, RIGHT, NONE }
 
-#var my_data = Network.players[name]
+var my_data
 
 slave var slave_position = Vector2()
 slave var slave_movement = Direction.NONE
-slave var my_skin = ""
 
 func _ready():
+	my_data = Network.players[int(name)]
 	if is_network_master():
+		$Selection.visible = true
 		$up.visible = true
 		$down.visible = true
 		$left.visible = true
 		$right.visible = true
-		for skin in skins.size():
-			if !Network.taken.has(skins[skin]):
-				$Skin.animation = skins[skin]
-				Network.taken.append(skins[skin])
-				my_skin = skins[skin]
-				for player in Network.players:
-					if player != get_tree().get_network_unique_id():
-						rset_id(player, 'my_skin', my_skin)
-				return
-	else:
-		$Skin.animation = my_skin
-		Network.taken.append(my_skin)
+		
+func _process(delta):
+		vis_skin(my_data["skin"])
 
-func init(nickname, start_position, is_slave):
+func init(nickname, start_skin, is_slave):
 	$Nickname.text = nickname
-	global_position = start_position
+	vis_skin(start_skin)
 	if is_slave:
 		pass
 
@@ -40,7 +32,15 @@ func _physics_process(delta):
 	var direction = Direction.NONE
 	if is_network_master():
 		$Camera2D.current = true
-		if Input.is_action_pressed('ui_left') or $left.is_pressed():
+		if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right"):
+			direction = Direction.UPRIGHT
+		elif Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left"):
+			direction = Direction.UPLEFT
+		elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_right"):
+			direction = Direction.DOWNRIGHT
+		elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left"):
+			direction = Direction.DOWNLEFT
+		elif Input.is_action_pressed('ui_left') or $left.is_pressed():
 			direction = Direction.LEFT
 		elif Input.is_action_pressed('ui_right') or $right.is_pressed():
 			direction = Direction.RIGHT
@@ -57,46 +57,62 @@ func _physics_process(delta):
 		_move(slave_movement)
 		position = slave_position
 	
-	#if get_tree().is_network_server():
-	#	Network.update_position(int(name), position)
 
 func _move(direction):
 	match direction:
 		Direction.NONE:
 			return
+		Direction.UPRIGHT:
+			move_and_slide(Vector2(1, -1).normalized()*SPEED)
+		Direction.UPLEFT:
+			move_and_slide(Vector2(-1, -1).normalized()*SPEED)
+		Direction.DOWNRIGHT:
+			move_and_slide(Vector2(1, 1).normalized()*SPEED)
+		Direction.DOWNLEFT:
+			move_and_slide(Vector2(-1, 1).normalized()*SPEED)
 		Direction.UP:
-			move_and_collide(Vector2(0, -SPEED))
+			move_and_slide(Vector2(0, -SPEED))
 		Direction.DOWN:
-			move_and_collide(Vector2(0, SPEED))
+			move_and_slide(Vector2(0, SPEED))
 		Direction.LEFT:
-			move_and_collide(Vector2(-SPEED, 0))
+			move_and_slide(Vector2(-SPEED, 0))
 		Direction.RIGHT:
-			move_and_collide(Vector2(SPEED, 0))
+			move_and_slide(Vector2(SPEED, 0))
 
-func change_skin(colour):
-	if is_network_master():
-		if !Network.taken.has(colour):
-			$Skin.animation = colour
-			Network.taken.erase(my_skin)
-			Network.taken.append(colour)
-			my_skin = colour
-			for player in Network.players:
-				if player != get_tree().get_network_unique_id():
-					rset_id(player, 'my_skin', my_skin)
-					return
-
+func vis_skin(colour):
+	$Skins/blue.visible = false
+	$Skins/green.visible = false
+	$Skins/pink.visible = false
+	$Skins/red.visible = false
+	$Skins/white.visible = false
+	$Skins/yellow.visible = false
+	$Skins/dew.visible = false
+	get_node("Skins/" + colour).visible = true
+	$Normal.disabled = true
+	$Dew.disabled = true
+	if colour == "dew":
+		$Nickname.rect_position = Vector2(-128, -66)
+		if is_network_master():
+			$Dew.disabled = false
+	else:
+		$Nickname.rect_position = Vector2(-128, -48)
+		if is_network_master():
+			$Normal.disabled = false
 
 func _on_blue_pressed():
-	change_skin("blue")
+	Network.change_skin(int(name), my_data["skin"], "blue")
 
 func _on_green_pressed():
-	change_skin("green")
+	Network.change_skin(int(name), my_data["skin"], "green")
 
 func _on_pink_pressed():
-	change_skin("pink")
+	Network.change_skin(int(name), my_data["skin"], "pink")
 
 func _on_red_pressed():
-	change_skin("red")
+	Network.change_skin(int(name), my_data["skin"], "red")
 
 func _on_yellow_pressed():
-	change_skin("yellow")
+	Network.change_skin(int(name), my_data["skin"], "yellow")
+
+func _on_dew_pressed():
+	Network.change_skin(int(name), my_data["skin"], "dew")
